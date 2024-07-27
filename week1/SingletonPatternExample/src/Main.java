72e7b3b3ff72c24a90c5b621db0a88dd94285a65
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-07-28 02:21:59 +0530

    singleton class added

--- week1/SingletonPatternExample/src/Main.java
+++ week1/SingletonPatternExample/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger l1 = Logger.getLogger();
+        Logger l2 = Logger.getLogger();
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l1 == l2); // prints true, indicating
+        // only one instance of the singleton class is created
+    }
+}
+
+class Logger {
+    private static final Logger logger = new Logger();
+
+    private Logger() {
+    }
+
+    public static Logger getLogger() {
+        return logger;
     }
 }
