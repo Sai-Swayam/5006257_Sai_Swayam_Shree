31300c273b6518282f7efd457d61a9d3831b2682
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-07-29 00:32:50 +0530

    factory method design pattern implemented

--- week1/FactoryMethodPatternExample/src/Main.java
+++ week1/FactoryMethodPatternExample/src/Main.java
@@ -1,5 +1,64 @@
+import javax.print.Doc;
+
+interface Document {
+    void show();
+}
+
+class WordDocument implements Document {
+    @Override
+    public void show() {
+        System.out.println("Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    @Override
+    public void show() {
+        System.out.println("Pdf Document");
+    }
+}
+
+class ExcelDocument implements Document {
+    @Override
+    public void show() {
+        System.out.println("Excel Document");
+    }
+}
+
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        
+        DocumentFactory wordDocumentFactory = new WordDocumentFactory();
+        Document wordDocument = wordDocumentFactory.createDocument();
+        wordDocument.show();
+
+        DocumentFactory pdfDocumentFactory = new PdfDocumentFactory();
+        Document pdfDocument = pdfDocumentFactory.createDocument();
+        pdfDocument.show();
+
+        DocumentFactory excelDocumentFactory = new ExcelDocumentFactory();
+        Document excelDocument = excelDocumentFactory.createDocument();
+        excelDocument.show();
     }
 }
