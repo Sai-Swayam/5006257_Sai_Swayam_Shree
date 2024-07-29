aa98402da3b589800b52403290b0482c871874e7
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-07-29 11:34:11 +0530

    Builder Pattern implemented

--- week1/Design Patterns/BuilderPatternExample/src/Computer.java
+++ week1/Design Patterns/BuilderPatternExample/src/Computer.java
@@ -3,13 +3,43 @@ public class Computer {
     int RAM;
     int Storage;
 
-    public Computer(String CPU, int RAM, int storage) {
-        this.CPU = CPU;
-        this.RAM = RAM;
-        Storage = storage;
+    public Computer(Builder builder) {
+        this.CPU = builder.CPU;
+        this.RAM = builder.RAM;
+        Storage = builder.Storage   ;
     }
 
-    public static class ComputerBuilder {
+    @Override
+    public String toString(){
+        return new String("CPU: " + this.CPU + "\nRAM: " + this.RAM + "\nStorage: " + this.Storage);
+    }
 
+    public static class Builder {
+        private String CPU;
+        private int RAM;
+        private int Storage;
+
+        public Builder setCPU(String CPU) {
+            this.CPU = CPU;
+            return this;
+        }
+
+        public Builder setRAM(int RAM) {
+            this.RAM = RAM;
+            return this;
+        }
+
+        public Builder setStorage(int Storage) {
+            this.Storage = Storage;
+            return this;
+        }
+
+        public Computer computerBuilder(){
+            return new Computer(this);
+        }
+
+//        public Builder getInstance() {
+//            return new Builder();
+//        }
     }
 }
--- week1/Design Patterns/BuilderPatternExample/src/Main.java
+++ week1/Design Patterns/BuilderPatternExample/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Computer ASUS_TUF = new Computer.Builder().setCPU("AMD Ryzen 5 4600H").setRAM(8).setStorage(512).computerBuilder();
+        System.out.println(ASUS_TUF);
     }
 }
