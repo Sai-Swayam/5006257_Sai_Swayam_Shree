6a16a32793d95050aea55dbefcfa88ecbeb9cb72
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-07-29 10:42:21 +0530

    folder restructure

---
+++ week1/Design Patterns/BuilderPatternExample/src/Computer.java
@@ -0,0 +1,15 @@
+public class Computer {
+    String CPU;
+    int RAM;
+    int Storage;
+
+    public Computer(String CPU, int RAM, int storage) {
+        this.CPU = CPU;
+        this.RAM = RAM;
+        Storage = storage;
+    }
+
+    public static class ComputerBuilder {
+
+    }
+}
